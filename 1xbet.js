1473142566b672e1f5087397a964829f3ee58ad7
Ahmedzxere <167022737+Ahmedzxere@users.noreply.github.com>
2024-04-14 23:32:29 +0200

    Update 1xbet.js

--- 1xbet.js
+++ 1xbet.js
@@ -2,12 +2,11 @@
 if (t.data.search('"OnRegistration"') != -1) {
 	o.onreceive(t.data);
 }
-if(t.data.search('"OnBet"') != -1){
+if(t.data.search('"OnBet"') != -1) {
     const data = JSON.parse(t.data.slice(0, -1));
-	}else{
-	}
-    o.onreceive('{"type":1,"target":"OnBet","arguments":[{"ok":false,"l":'+data.arguments[0].l+',"t":0,"ec":'+data.arguments[0].ec+',"et":"'+mseg+'","ts":'+data.arguments[0].ts+'}]}\x1e')
-  }else{
+    o.onreceive('{"type":1,"target":"OnBet","arguments":[{"ok":true,"l":'+data.arguments[0].l+',"t":0,"ec":'+data.arguments[0].ec+',"et":"","ts":'+data.arguments[0].ts+'}]}\x1e');
+} else {
+}
   try{
    setTimeout(() => {
  o.onreceive(t.data);
