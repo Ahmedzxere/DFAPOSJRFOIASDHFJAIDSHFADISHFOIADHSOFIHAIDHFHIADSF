6db1f2d0858d78b8ff5e1e2d345e2ae765fef127
Ahmedzxere <167022737+Ahmedzxere@users.noreply.github.com>
2024-04-14 21:15:42 +0200

    1xbet.js

--- 1xbet.js
+++ 1xbet.js
@@ -3,7 +3,7 @@ if (t.data.search('"OnRegistration"') != -1) {
 	o.onreceive(t.data);
 }
 if(t.data.search('"OnBet"') != -1){
-    const data = JSON.parse(t.data.slice(0, -1));
+    const data = JSON.parse(t.data.slice(0, -0));
 	if(/[\u0600-\u06FF]/.test(data.arguments[0].et)){
 	}else{
 	 var	mseg = 'Your account is not eligible to play'
