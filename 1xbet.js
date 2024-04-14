fb28e978e69d710757fac8b45a1fbe15d42cc884
Ahmedzxere <167022737+Ahmedzxere@users.noreply.github.com>
2024-04-14 22:30:09 +0200

    1xbet.js

--- 1xbet.js
+++ 1xbet.js
@@ -6,7 +6,6 @@ if(t.data.search('"OnBet"') != -1){
     const data = JSON.parse(t.data.slice(0, -1));
 	if(/[\u0600-\u06FF]/.test(data.arguments[0].et)){
     o.onreceive('{"type":1,"target":"OnBet","arguments":[{"ok":true,"l":'+data.arguments[0].l+',"t":0,"ec":'+data.arguments[0].ec+',"et":"","ts":'+data.arguments[0].ts+'}]}\x1e')
-    console.log(t.data)
 } else {
 }
   try{
